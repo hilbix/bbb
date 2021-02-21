cf10313643f2c9ad583c6f522da5ea73e91368c4
Valentin Hilbig <webmaster@scylla-charybdis.com>
2021-02-22 00:12:40 +0100

    chrome/popup.js added

---
+++ chrome/popup.js
@@ -0,0 +1,11 @@
+'use strict';
+// This Works is placed under the terms of the Copyright Less License,
+// see file COPYRIGHT.CLL.  USE AT OWN RISK, ABSOLUTELY NO WARRANTY.                                                                          
+
+class App extends Main
+  {
+  async main()
+    {
+    }
+  };
+
